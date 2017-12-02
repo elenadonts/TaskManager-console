261d3f9b386e7120031a5ddc9421380c30869a35
elenadonts <elenadonts@gmail.com>
2017-12-02 21:12:49 +0200

    possibility of storing tasks in calendar added

---
+++ src/com/dontsova/elena/Tasks.java
@@ -0,0 +1,36 @@
+package com.dontsova.elena;
+
+import java.util.*;
+
+public class Tasks {
+    public static Iterable<Task> incoming(Iterable<Task> tasks, Date start, Date end){
+        ArrayList<Task> incomingTasks = new ArrayList<>();
+        for (Task t : tasks) {
+            Date nextTime = t.nextTimeAfter(start);
+            if (nextTime != null && (nextTime.before(end) || nextTime.equals(end))) {
+                incomingTasks.add(t);
+            }
+        }
+        return incomingTasks;
+    }
+    public static SortedMap<Date, Set<Task>> calendar(Iterable<Task> tasks, Date start, Date end){
+        Iterable<Task> incomingTasks = incoming(tasks, start, end);
+        TreeMap<Date, Set<Task>> calendar = new TreeMap<>();
+
+        for (Task t : incomingTasks){
+            Date nextTimeAfter = t.nextTimeAfter(start);
+            while (nextTimeAfter!= null && (nextTimeAfter.before(end) || nextTimeAfter.equals(end))){
+                if (calendar.containsKey(nextTimeAfter)){
+                    calendar.get(nextTimeAfter).add(t);
+                }
+                else {
+                    HashSet<Task> oneDateTasks = new HashSet<>();
+                    oneDateTasks.add(t);
+                    calendar.put(nextTimeAfter,oneDateTasks);
+                }
+                nextTimeAfter = t.nextTimeAfter(nextTimeAfter);
+            }
+        }
+        return calendar;
+    }
+}
